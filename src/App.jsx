e670646bb9a737435de3e149c2ebfb22aa51fa40
sikander-afzaal <sikander.aaa@gmail.com>
2023-02-28 11:01:45 +0500

    asdasdasd

--- src/App.jsx
+++ src/App.jsx
@@ -422,29 +422,32 @@ export default App;
 const InputComp = ({ label, name, value, handler, col, info, sub, gold }) => {
   return (
     <div className={`colRow ${col ? "inputCol" : ""}`}>
-      <label htmlFor={label}>
-        {label}
-        {info && (
-          <>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="black"
-              className="info-ico"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
-              />
-            </svg>
-            <p className="info-text">{info}</p>
-          </>
-        )}
-      </label>
-      {sub && <h6>{sub}</h6>}
+      <div className="top-label">
+        {" "}
+        <label htmlFor={label}>
+          {label}
+          {info && (
+            <>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="black"
+                className="info-ico"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"
+                />
+              </svg>
+              <p className="info-text">{info}</p>
+            </>
+          )}
+        </label>
+        {sub && <h6>{sub}</h6>}
+      </div>
       <div className="col-div">
         <div className="flex-div">
           <p>$</p>
